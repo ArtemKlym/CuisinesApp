bc2e05314c63991ac6761e453ff1fb0c5e8a1f9b
Artem <artemklymenko892@gmail.com>
2025-07-24 09:48:22 +0300

    Create use cases for search

---
+++ feature/search/domain/src/main/java/com/artemklymenko/search/domain/use_cases/GetAllRecipeUseCase.kt
@@ -0,0 +1,19 @@
+package com.artemklymenko.search.domain.use_cases
+
+import com.artemklymenko.common.utils.NetworkResult
+import com.artemklymenko.search.domain.model.RecipeDomain
+import com.artemklymenko.search.domain.repository.SearchRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class GetAllRecipeUseCase @Inject constructor(
+    private val searchRepository: SearchRepository
+){
+
+    operator fun invoke(searchQuery: String) = flow<NetworkResult<List<RecipeDomain>>> {
+        val response = searchRepository.getRecipes(searchQuery)
+        emit(response)
+    }.flowOn(Dispatchers.IO)
+}
