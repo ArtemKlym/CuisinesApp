a3890931c2f6e394868ca5175f001b7c9cd3e32b
Artem <artemklymenko892@gmail.com>
2025-07-25 16:47:23 +0300

    Create FavouriteScreen

---
+++ feature/search/ui/src/main/java/com/artemklymenko/search/ui/screens/favoutire_recipes/FavouriteEvent.kt
@@ -0,0 +1,12 @@
+package com.artemklymenko.search.ui.screens.favoutire_recipes
+
+import com.artemklymenko.search.domain.model.RecipeDomain
+
+sealed interface FavouriteEvent {
+    data object AlphabeticalSort: FavouriteEvent
+    data object LessIngredientSort: FavouriteEvent
+    data object ResetSort: FavouriteEvent
+    data class ShowDetails(val id: String): FavouriteEvent
+    data class DeleteRecipe(val recipeDomain: RecipeDomain): FavouriteEvent
+    data class GoToDetails(val id: String): FavouriteEvent
+}
