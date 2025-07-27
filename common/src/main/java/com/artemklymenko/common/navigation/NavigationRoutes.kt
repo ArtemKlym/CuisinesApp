6bbb05b22afac5d497430a1cca4d2b2f06e3816d
Artem <artemklymenko892@gmail.com>
2025-07-27 15:00:53 +0300

    Add navigation to favourite screen and media player

--- common/src/main/java/com/artemklymenko/common/navigation/NavigationRoutes.kt
+++ common/src/main/java/com/artemklymenko/common/navigation/NavigationRoutes.kt
@@ -5,8 +5,14 @@ sealed class NavigationRoute(val route: String) {
     data object RecipeDetails: NavigationRoute("/recipe_details/{id}") {
         fun sendId(id: String) = "/recipe_details/$id"
     }
+    data object FavouriteScreen: NavigationRoute("/favourite")
+
+    data object MediaPlayer: NavigationRoute("/player/{video_id}") {
+        fun sendUrl(videoId: String) = "/player/$videoId"
+    }
 }
 
 sealed class NavigationSubGraphRoute(val route: String) {
     data object Search: NavigationSubGraphRoute("/search")
+    data object MediaPlayer: NavigationSubGraphRoute("/media_player")
 }
